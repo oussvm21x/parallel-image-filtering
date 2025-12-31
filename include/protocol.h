5150602767d6d0d8ff4b6f066b702893790fffd7
Khelifa Saadallah Oussama <oussakhema00@gmail.com>
2025-12-31 04:22:38 +0100

    Updating protocol
    now we gonna deal with a table of requests

--- include/protocol.h
+++ include/protocol.h
@@ -12,7 +12,9 @@
 #define SHM_NAME "/shm_requests" // Shared memory name
 #define MAX_PATH_LENGTH 256      // Maximum length for file paths
 #define FIFO_RESPONSE_TEMPLATE "/tmp/fifo_rep_%d" // Template for response FIFO names 
-
+#define SEM_MUTEX_NAME "/sem_mutex" // Semaphore name for mutual exclusion
+#define SEM_EMPTY_NAME "/sem_empty" // Semaphore name for empty slots
+#define SEM_FULL_NAME "/sem_full"   // Semaphore name for full slots
 
 
 /* --- 2. Request Structure ---*/
@@ -24,6 +26,14 @@ int filtre ;
 int parametres [5];
 } filter_request_t ;
 
+
+/* --- 3. Shared Memory Structure ---*/
+typedef struct request_table_t {
+    size_t in;    // Index for the next write
+    size_t out;   // Index for the next read
+    size_t size;  // Size of the buffer
+    filter_request_t buffer[]; // Circular buffer for requests
+} request_table_t ;
 
 
 #endif // PROTOCOL_H
